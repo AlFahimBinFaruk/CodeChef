c6d20c6201c4797545ab00da76a189dfbadf57c7
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2023-02-04 20:47:46 +0600

    Update Construct_a_tree.cpp

--- Construct_a_tree.cpp
+++ Construct_a_tree.cpp
@@ -1,46 +1,53 @@
 // https://practice.geeksforgeeks.org/problems/construct-tree-1/
 // perorder traversal is used to find parent.
 // inorder traversal is used to find left,right child of that parent...
-/* 
+/*
 we know that the left most item in preorder is the root node.so we will find it in the inorder array.when we find it the left items of
 that array will be left child and right items will be right child..
 we will repeat the same procejure untill the tree is empty then return its root.
 */
-class Solution{
-    public:
+class Solution
+{
+public:
 	int preIndex = 0;
-    int searchIn(int arr[],int start,int end,int value){
-        for(int i=start; i<=end; i++){
-            if(arr[i] == value){
-                return i;
-            }
-        }
+	int searchIn(int arr[], int start, int end, int value)
+	{
+		for (int i = start; i <= end; i++)
+		{
+			if (arr[i] == value)
+			{
+				return i;
+			}
+		}
 	}
 
-
-	Node* buildTreeUtil(int in[],int pre[],int inStart,int inEnd){
-		if(inStart > inEnd){
+	Node *buildTreeUtil(int in[], int pre[], int inStart, int inEnd)
+	{
+		if (inStart > inEnd)
+		{
 			return NULL;
 		}
-		
-		Node* tNode = new Node(pre[preIndex]);
+
+		Node *tNode = new Node(pre[preIndex]);
 		preIndex++;
-		
+
 		// if tree has no childs
-		if(inStart == inEnd){
+		if (inStart == inEnd)
+		{
 			return tNode;
 		}
-		
-		int inIndex = searchIn(in,inStart,inEnd,tNode->data);
-		
+
+		int inIndex = searchIn(in, inStart, inEnd, tNode->data);
+
 		// build left tree
-		tNode->left = buildTreeUtil(in,pre,inStart,inIndex - 1);
+		tNode->left = buildTreeUtil(in, pre, inStart, inIndex - 1);
 		// build right tree
-		tNode->right = buildTreeUtil(in,pre,inIndex + 1,inEnd);
-		
+		tNode->right = buildTreeUtil(in, pre, inIndex + 1, inEnd);
+
 		return tNode;
 	}
-    Node* buildTree(int in[],int pre[], int n){
-        return buildTreeUtil(in,pre,0,n - 1);
-    }
+	Node *buildTree(int in[], int pre[], int n)
+	{
+		return buildTreeUtil(in, pre, 0, n - 1);
+	}
 };
